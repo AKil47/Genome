2170213591f5921174651ea96e5d836cc395b8b9
AKil47 <amanivannan@outlook.com>
2017-12-03 18:58:48 -0600

    Fixed csv formatting

--- src/analyze/fileExporter.java
+++ src/analyze/fileExporter.java
@@ -3,6 +3,10 @@ package analyze;
 
 import analyze.functions.varExtractor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 class fileExporter {
     //New varExtractor Object(Needs to be constructed)
     private varExtractor varExtractor = new varExtractor();
@@ -15,7 +19,7 @@ class fileExporter {
     }
 
     public void csvExport() {
-        /*
+        //Set output to file
         PrintStream o = null;
         try {
             o = new PrintStream(new File("Export.csv"));
@@ -24,38 +28,44 @@ class fileExporter {
         }
         PrintStream console = System.out;
         System.setOut(o);
-        */
+
+
         //HEADING
-        System.out.println("Key,Action,Time,,,Duration,,,Seek,,,Latency");
+        System.out.println("Key,Action,Time,,,Duration,,Seek,,Latency");
         //Initialize Latency ArrayLists and Incrementing Variable
         int x = 1;
         varExtractor.initLatency();
         try {
             for (int i = 0; i < varExtractor.getKey().size(); i++) {
                 if ((i & 1) == 0) {
-                    //EVEN CODE (SEEK)
+                    //EVEN CODE (SEEK)(DOWN)
                     if(i >= 2 && i != varExtractor.getSeek().size()) {
                         //TODO Add Latency to top line
                         if ((x & 1) == 0) {
                             //Add Latency
-                            System.out.println((varExtractor.getKey().get(i) + "," + varExtractor.getAction().get(i) + "," + varExtractor.getTime().get(i) + ",,,,,") + (varExtractor.getSeek(i)) + (",,," + varExtractor.getLatency(i-x)));
+                            System.setOut(o);
+                            System.out.println((varExtractor.getKey().get(i) + "," + varExtractor.getAction().get(i) + "," + varExtractor.getTime().get(i) + ",,,,,") + (varExtractor.getSeek(i)) + (",," + varExtractor.getLatency(i-x)));
                         }
                         else {
                             //No Latency
+                            System.setOut(o);
                             System.out.println((varExtractor.getKey().get(i) + "," + varExtractor.getAction().get(i) + "," + varExtractor.getTime().get(i) + ",,,,,") + (varExtractor.getSeek(i)));
                         }
                     } else {
+                        System.setOut(o);
                         System.out.println((varExtractor.getKey().get(i) + "," + varExtractor.getAction().get(i) + "," + varExtractor.getTime().get(i) + ",,,,,"));
                     }
 
                 } else {
-                    //ODD CODE (DURATION)
+                    //ODD CODE (DURATION)(UP)
                     if ((x & 1) == 0) {
                         //Add Latency
-                        System.out.println((varExtractor.getKey().get(i) + "," + varExtractor.getAction().get(i) + "," + varExtractor.getTime().get(i) + ",,,") + (varExtractor.getDuration(i)) + (",,," + varExtractor.getLatency(i-x)));
+                        System.setOut(o);
+                        System.out.println((varExtractor.getKey().get(i) + "," + varExtractor.getAction().get(i) + "," + varExtractor.getTime().get(i) + ",,,") + (varExtractor.getDuration(i)) + (",,,," + varExtractor.getLatency(i-x)));
                     }
                     else {
                         //No Latency
+                        System.setOut(o);
                         System.out.println((varExtractor.getKey().get(i) + "," + varExtractor.getAction().get(i) + "," + varExtractor.getTime().get(i) + ",,,") + (varExtractor.getDuration(i)));
                     }
                     x++;
@@ -64,7 +74,8 @@ class fileExporter {
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            System.setOut(console);
+            System.out.println(e);
         }
 
 
